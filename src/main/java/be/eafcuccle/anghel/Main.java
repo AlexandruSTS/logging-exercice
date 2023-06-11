8ad8bfe890690bcd7ad77e33f7eb3aa2acb169cb
Alexandru Anghel <alexandru@ditar.io>
2023-06-11 09:45:43 +0200

    rolling policy added

--- src/main/java/be/eafcuccle/anghel/Main.java
+++ src/main/java/be/eafcuccle/anghel/Main.java
@@ -1,5 +1,17 @@
-package be.eafcuccle.anghel;public class Main {
+package be.eafcuccle.anghel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        logger.trace("Message TRACE");
+        logger.debug("Message DEBUG");
+        logger.info("Message INFO");
+        logger.warn("Message WARN");
+        logger.error("Message ERROR");
+
     }
 }
